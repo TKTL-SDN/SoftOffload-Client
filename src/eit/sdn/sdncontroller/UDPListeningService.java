7b69daf8baabc1c61f2c9711dc7415ee3b8a1ddf
Yanhe <yanhe.liu@cs.helsinki.fi>
2015-04-15 18:18:14 +0300

    Add/modify licence declaration

--- src/eit/sdn/sdncontroller/UDPListeningService.java
+++ src/eit/sdn/sdncontroller/UDPListeningService.java
@@ -1,5 +1,5 @@
 /**
-*    Copyright 2013 University of Helsinki
+*    Copyright 2015 University of Helsinki
 *
 *    Licensed under the Apache License, Version 2.0 (the "License"); you may
 *    not use this file except in compliance with the License. You may obtain
